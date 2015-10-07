ba7f50c56332f01945a59877f427cff1bb7b1693
nikobraz <nikita_yarushin@mail.ru>
2015-10-07 15:39:19 +0500

    работа над мин и макс зп

--- init.py
+++ init.py
@@ -18,26 +18,27 @@ def parse(html):
         cols = row.find_all('td')
         title = cols[1].a.text
         link = cols[1].a['href']
-        salary = cols[1].find('div', class_='job_price').span
-        if not (salary and salary.string):
+        min_salary = cols[1].find('div', class_='job_price').text.strip()
+        print(min_salary)
+'''        if not (min_salary and min_salary.string):
             salary = '0'
             countzero += 1
         else:
-            salary = salary.string
+            min_salary = min_salary.string
+            print(min_salary)
         vac.append({
             'title': title,
             'link': link,
-            'salary': int(salary.replace(' ', '')),
+            'salary': int(min_salary.replace(' ', '')),
         })
     medsal = 0
     for vacancy in vac:
         print(vacancy)
         medsal += vacancy['salary']
-    print('Средняя зарплата по больнице:', int(medsal / (len(vac) - countzero)), 'рублей')
+    print('Средняя зарплата по больнице:', int(medsal / (len(vac) - countzero)), 'рублей')'''
 
 
 def main():
-    csv.
     parse(get_data('http://74.ru/job/vacancy/?Where%5B%5D=1&Search=1&Query=%F1%E8%F1%F2%E5%EC%ED%FB%E9+%E0%E4%EC%E8%ED'
                    '%E8%F1%F2%F0%E0%F2%EE%F0&SalaryMin=%C7%E0%F0%EF%EB%E0%F2%E0+%EE%F2%2C+%F0%F3%E1&BranchID='))
     input('Press Any Key To Exit')
